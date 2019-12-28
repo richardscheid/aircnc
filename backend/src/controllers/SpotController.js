85224fd12310b3dc6c2f035d902f1cb935c477e6
richardscheid <rscheid@live.com>
2019-12-28 17:44:17 -0300

    creating find techs method

--- backend/src/controllers/SpotController.js
+++ backend/src/controllers/SpotController.js
@@ -2,6 +2,15 @@ const Spot = require('../models/Spot');
 const User = require('../models/User');
 
 module.exports = {
+
+    async index(req, res) {
+        const { tech } = req.query;
+
+        const spots = await Spot.find({ techs: tech });
+        
+        return res.json(spots);
+    },
+
     async store(req, res) {
         const { filename } = req.file;
         const { company, techs, price} = req.body;
@@ -20,5 +29,5 @@ module.exports = {
         })
 
         return res.json(spot); 
-    }
+    },
 };
