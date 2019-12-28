263f7fdc2788329ddebcfd43bd415786dd5948cf
richardscheid <rscheid@live.com>
2019-12-28 15:40:45 -0300

    adding moongose path

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,11 @@ const routes = require('./routes');
 
 const app = express();
 
+mongoose.connect('mongodb+srv://onmistack:onmistack@cluster0-mzlt4.mongodb.net/aircnc?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
 app.use(express.json());
 app.use(routes);
 app.listen(3030);
