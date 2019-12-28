7c9c056060f5b1cb9c155cbec0db07da10a90c2a
richardscheid <rscheid@live.com>
2019-12-28 16:53:39 -0300

    creating config for images

---
+++ backend/src/config/upload.js
@@ -0,0 +1,14 @@
+const multer = require('multer');
+const path = require('path');
+
+module.exports = {
+    storage: multer.diskStorage({
+        destination: path.resolve(__dirname, '..', '..', 'uploads'),
+        filename: (req, file, callBack) => {
+            const ext = path.extname(file.originalname);
+            const name = path.basename(file.originalname, ext);
+
+            callBack(null, `${name}-${Date.now}${ext}`);
+        },
+    }),
+};
