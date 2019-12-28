53233e8c55576cc7b6f1a7a9181fb9b07c42e8fd
richardscheid <rscheid@live.com>
2019-12-28 17:24:31 -0300

    adding creation spots at controller

--- backend/src/controllers/SpotController.js
+++ backend/src/controllers/SpotController.js
@@ -1,8 +1,24 @@
+const Spot = require('../models/Spot');
+const User = require('../models/User');
+
 module.exports = {
     async store(req, res) {
-        console.log(req.body);
-        console.log(req.file);
+        const { filename } = req.file;
+        const { company, techs, price} = req.body;
+        const { user_id } = req.headers;
 
-        return res.json({ ok: true }); 
+        const user = await User.findById(user_id);
+        
+        if (!user) return res.status(400).json({ error: 'User does not exists!' });
+
+        const spot = await Spot.create({
+            user: user_id,
+            thumbnail: filename,
+            company,
+            price,
+            techs: techs.split(',').map(tech => tech.trim()),
+        })
+
+        return res.json(spot); 
     }
 };
