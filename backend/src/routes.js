f6ba5f70f8864077c25afcfabc5045829d8469ed
richardscheid <rscheid@live.com>
2019-12-28 18:17:24 -0300

    adding new booking route

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,6 +4,7 @@ const uploadConfig = require('./config/upload');
 
 const SpotController = require('./controllers/SpotController');
 const SessionController = require('./controllers/SessionController');
+const BookingController = require('./controllers/BookingController');
 const DashboardController = require('./controllers/DashboardController');
 
 const routes = express.Router();
@@ -15,5 +16,7 @@ routes.get('/spots', SpotController.index);
 routes.post('/spots', upload.single('thumbnail'), SpotController.store);
 
 routes.get('/dashboard', DashboardController.show);
+
+routes.post('/spots/:spot_id/bookings', BookingController.store);
 
 module.exports = routes;
