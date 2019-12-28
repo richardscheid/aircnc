53f15eff798c006458c41d334ddcb3035dff12fe
richardscheid <rscheid@live.com>
2019-12-28 17:56:52 -0300

    adding route for dashboard

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,8 +2,9 @@ const express = require('express');
 const multer = require('multer');
 const uploadConfig = require('./config/upload');
 
-const SessionController = require('./controllers/SessionController');
 const SpotController = require('./controllers/SpotController');
+const SessionController = require('./controllers/SessionController');
+const DashboardController = require('./controllers/DashboardController');
 
 const routes = express.Router();
 const upload = multer(uploadConfig);
@@ -12,5 +13,7 @@ routes.post('/sessions', SessionController.store);
 
 routes.get('/spots', SpotController.index);
 routes.post('/spots', upload.single('thumbnail'), SpotController.store);
+
+routes.get('/dashboard', DashboardController.show);
 
 module.exports = routes;
