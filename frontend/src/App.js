87651867f9d55f65c967461b22e7179986e7009d
richardscheid <rscheid@live.com>
2019-12-30 16:14:33 -0300

    building the home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import './App.css';
+import logo from './assets/logo.svg';
 
 function App() {
   return (
-    
+    <div className="container">
+      <img src={logo} alt="AirCnC" className="src"/>
+
+      <div className="content">
+        <p>
+          Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para a sua empresa.
+        </p>
+
+        <from>
+          <label htmlFor="email">E-mail *</label>
+          <input 
+            id="email"
+            type="email" 
+            placeholder="Seu melhor e-mail"
+          />
+        </from>
+      </div>
+    </div>
   );
 }
 
