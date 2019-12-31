26e0690397434d3699056f9ab487a2e656ab5163
richardscheid <rscheid@live.com>
2019-12-31 17:30:54 -0300

    refactoring

--- backend/src/controllers/SpotController.js
+++ backend/src/controllers/SpotController.js
@@ -1,33 +1,36 @@
-const Spot = require('../models/Spot');
-const User = require('../models/User');
+const Spot = require("../models/Spot");
+const User = require("../models/User");
 
 module.exports = {
+  async index(req, res) {
+    const { tech } = req.query;
 
-    async index(req, res) {
-        const { tech } = req.query;
+    const spots = await Spot.find({ techs: tech });
 
-        const spots = await Spot.find({ techs: tech });
-        
-        return res.json(spots);
-    },
+    return res.json(spots);
+  },
 
-    async store(req, res) {
-        const { filename } = req.file;
-        const { company, techs, price} = req.body;
-        const { user_id } = req.headers;
+  async store(req, res) {
+    const { filename } = req.file;
+    const { company, techs, price } = req.body;
+    const { user_id } = req.headers;
 
-        const user = await User.findById(user_id);
-        
-        if (!user) return res.status(400).json({ error: 'User does not exists!' });
+    const user = await User.findById(user_id);
 
-        const spot = await Spot.create({
-            user: user_id,
-            thumbnail: filename,
-            company,
-            price,
-            techs: techs.split(',').map(tech => tech.trim()),
-        })
+    if (!user) {
+      return res.status(400).json({ error: "User does not exists!" });
+    }
 
-        return res.json(spot); 
-    },
+    console.log('passou aqui');
+
+    const spot = await Spot.create({
+      user: user_id,
+      thumbnail: filename,
+      company,
+      price,
+      techs: techs.split(",").map(tech => tech.trim())
+    });
+
+    return res.json(spot);
+  }
 };
