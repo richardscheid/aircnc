51cb9e570ce5991e181f9df916e1bffbfe310eeb
richardscheid <rscheid@live.com>
2019-12-31 15:45:08 -0300

    creating the new routes

---
+++ frontend/src/routes.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import New from "./pages/New";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+
+export default function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Login} />
+        <Route path="/new" component={New} />
+        <Route path="/dashboard" component={Dashboard} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
