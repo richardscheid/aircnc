9164a0d990a5c74ef921b64f54511a8f388e8bb9
richardscheid <rscheid@live.com>
2020-01-05 21:54:41 -0300

    configuration of socketio

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,13 +10,25 @@ const app = express();
 const server = http.Server(app);
 const io = scoketio(server);
 
-io.on('connection', socket => {
-    console.log(socket.id);
-})
-
 mongoose.connect('mongodb+srv://onmistack:onmistack@cluster0-mzlt4.mongodb.net/aircnc?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
+  }
+);
+
+const connectedUsers = {};
+
+io.on('connection', socket => {
+  const { user_id } = socket.handshake.query;
+
+  connectedUsers[user_id] = socket.id;
+});
+
+app.use((req, res, next) => {
+  req.io = io;
+  req.connectedUsers = connectedUsers;
+
+  return next;
 });
 
 app.use(cors());
