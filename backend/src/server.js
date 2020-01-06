37198940c36c476b2555ef45b9fc43a5a4ae2284
richardscheid <rscheid@live.com>
2020-01-05 21:33:53 -0300

    adding socket to the server

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,8 +3,16 @@ const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
 const path = require('path');
+const http = require('http');
+const scoketio = require('socket.io');
 
 const app = express();
+const server = http.Server(app);
+const io = scoketio(server);
+
+io.on('connection', socket => {
+    console.log(socket.id);
+})
 
 mongoose.connect('mongodb+srv://onmistack:onmistack@cluster0-mzlt4.mongodb.net/aircnc?retryWrites=true&w=majority', {
     useNewUrlParser: true,
@@ -15,4 +23,5 @@ app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
-app.listen(3030);
+
+server.listen(3030);
