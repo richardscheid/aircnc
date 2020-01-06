af25a2afe538bc7ef447d44628f1445340421586
richardscheid <rscheid@live.com>
2020-01-06 19:58:12 -0300

    creating new routes for approval/rejection

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -6,6 +6,8 @@ const SpotController = require('./controllers/SpotController');
 const SessionController = require('./controllers/SessionController');
 const BookingController = require('./controllers/BookingController');
 const DashboardController = require('./controllers/DashboardController');
+const ApprovalController = require('./controllers/ApprovalController');
+const RejectionController = require('./controllers/RejectionController');
 
 const routes = express.Router();
 const upload = multer(uploadConfig);
@@ -18,5 +20,8 @@ routes.post('/spots', upload.single('thumbnail'), SpotController.store);
 routes.get('/dashboard', DashboardController.show);
 
 routes.post('/spots/:spot_id/bookings', BookingController.store);
+
+routes.post('/bookings/:booking_id/approvals', ApprovalController.store);
+routes.post('/bookings/:booking_id/rejections', RejectionController.store);
 
 module.exports = routes;
