9a3bdcdff0f3dbf49c2b34afc4586a3055a46cc5
richardscheid <rscheid@live.com>
2020-01-05 21:41:02 -0300

    creating socket to connect with backend

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,15 +1,20 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import api from "../../services/api";
+import api from '../../services/api';
+import socketio from 'socket.io-client';
 import './styles.css';
 
 export default function Dashboard() {
   const [spots, setSpots] = useState([]);
 
   useEffect(() => {
+    const socket = socketio('http://localhost:3030');
+  }, []);
+
+  useEffect(() => {
     async function loadSpots() {
-      const user_id = localStorage.getItem("user");
-      const response = await api.get("/dashboard", {
+      const user_id = localStorage.getItem('user');
+      const response = await api.get('/dashboard', {
         headers: { user_id }
       });
 
@@ -21,18 +26,20 @@ export default function Dashboard() {
 
   return (
     <>
-      <ul className="spot-list">
+      <ul className='spot-list'>
         {spots.map(spot => (
           <li key={spot._id}>
-            <header style={{ backgroundImage: `url(${spot.thumbnail_url})` }}></header>
+            <header
+              style={{ backgroundImage: `url(${spot.thumbnail_url})` }}>
+            </header>
             <strong>{spot.company}</strong>
             <span>{spot.price ? `R$${spot.price}/dia` : 'Free'}</span>
           </li>
         ))}
       </ul>
 
-      <Link to="/new">
-        <button className="btn">Cadastrar novo spot</button>
+      <Link to='/new'>
+        <button className='btn'>Cadastrar novo spot</button>
       </Link>
     </>
   );
